a7670835727fefd0879a638d71a00d97f6fca4b4
furaibo <furaiboxyz@gmail.com>
2024-08-19 17:41:55 +0900

    Initial commit

---
+++ src/test/java/com/furaibo/rfc3161timestampapi/Rfc3161TimeStampApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furaibo.rfc3161timestampapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rfc3161TimeStampApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

deeeeff65f57ad3de1c510164cc3c5196c07dbf4
furaibo <furaiboxyz@gmail.com>
2024-08-19 17:41:55 +0900

    Initial commit

---
+++ src/main/java/com/furaibo/rfc3161timestampapi/Rfc3161TimeStampApiApplication.java
@@ -0,0 +1,13 @@
+package com.furaibo.rfc3161timestampapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rfc3161TimeStampApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rfc3161TimeStampApiApplication.class, args);
+	}
+
+}

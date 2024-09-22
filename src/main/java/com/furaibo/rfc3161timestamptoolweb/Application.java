3f1a4d558287ee0fa4007a214f407638641e29c0
furaibo <furaiboxyz@gmail.com>
2024-09-22 20:43:38 +0900

    update project name & pom.xml

--- src/main/java/com/furaibo/rfc3161timestamptoolweb/Application.java
+++ src/main/java/com/furaibo/rfc3161timestamptoolweb/Application.java
@@ -1,13 +1,17 @@
-package com.furaibo.rfc3161timestampapi;
+package com.furaibo.rfc3161timestamptoolweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Rfc3161TimeStampApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Rfc3161TimeStampApiApplication.class, args);
+		try {
+			SpringApplication.run(Application.class, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/main/java/com/furaibo/rfc3161timestamptoolweb/ServletInitializer.java
+++ src/main/java/com/furaibo/rfc3161timestamptoolweb/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.furaibo.rfc3161timestampapi;
+package com.furaibo.rfc3161timestamptoolweb;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Rfc3161TimeStampApiApplication.class);
+		return application.sources(Application.class);
 	}
 
 }

3e964a1a407383a672a554421682aa0c356d0b35
furaibo <furaiboxyz@gmail.com>
2024-09-30 20:39:32 +0900

    add function of attaching files to single pdf

--- src/main/java/com/furaibo/rfc3161timestamptoolweb/service/PDFService.java
+++ src/main/java/com/furaibo/rfc3161timestamptoolweb/service/PDFService.java
@@ -5,12 +5,14 @@ import org.apache.commons.io.FileUtils;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDDocumentNameDictionary;
-import org.apache.pdfbox.pdmodel.PDEmbeddedFilesNameTreeNode;
-import org.apache.pdfbox.pdmodel.PageMode;
+import org.apache.pdfbox.pdmodel.*;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.common.filespecification.PDComplexFileSpecification;
 import org.apache.pdfbox.pdmodel.common.filespecification.PDEmbeddedFile;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -23,28 +25,32 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 
 @Service
 public class PDFService {
 
     // メンバ変数
-    @Value("${app.output.folder.file}")
-    private String outputFileRootPathStr;
-
     private final DateTimeFormatter dtFormat = DateTimeFormatter.ofPattern("yyyyMMddhhmmss");
     private Path outputFileRootPath;
     private Path outputFileFolderPath;
+    private Path tmpFileRootPath;
 
+    @Value("${app.output.folder.file}")
+    private String outputFileRootPathStr;
+
+    @Value("${app.tmp.folder.file}")
+    private String tmpFileRootPathStr;
+
+    @Value("${app.jp.font.file}")
+    private String jpFontPathStr;
 
     @PostConstruct
     public void init() {
         try {
             this.outputFileRootPath = Paths.get(outputFileRootPathStr);
+            this.tmpFileRootPath = Paths.get(tmpFileRootPathStr);
             this.outputFileFolderPath = this.outputFileRootPath.resolve(dtFormat.format(LocalDateTime.now()));
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -65,9 +71,9 @@ public class PDFService {
 
         // 出力先ファイルパス
         String outputFilePathStr = String.format(
-                "%s/%s.pdf",
-                this.outputFileFolderPath, dtFormat.format(LocalDateTime.now())
-            );
+            "%s/%s.pdf",
+            this.outputFileFolderPath, dtFormat.format(LocalDateTime.now())
+        );
         Path outputFilePath = Paths.get(outputFilePathStr);
 
         // PDFMergeUtilityの初期化
@@ -90,20 +96,18 @@ public class PDFService {
      * @param baseFilePath
      * @param attachFilePathList
      * @throws IOException
+     * @return Path
      */
-    public void attachFiles(
+    public Path attachFiles(
             Path baseFilePath, List<Path> attachFilePathList)
             throws IOException {
 
         // ディレクトリの作成
-        Files.createDirectories(this.outputFileFolderPath);
+        Files.createDirectories(this.tmpFileRootPath);
 
         // 出力先ファイルパス
-        String outputFilePathStr = String.format(
-                "%s/%s.pdf",
-                this.outputFileFolderPath, dtFormat.format(LocalDateTime.now())
-            );
-        Path outputFilePath = Paths.get(outputFilePathStr);
+        Path outputFilePath = this.tmpFileRootPath.resolve(
+                UUID.randomUUID().toString() + ".pdf");
 
         // DDocumentおよびツリーノードの初期化
         PDDocument document = Loader.loadPDF(baseFilePath.toFile());
@@ -155,6 +159,112 @@ public class PDFService {
         // ファイル保存
         document.save(outputFilePath.toString());
         document.close();
+
+        return outputFilePath;
+    }
+
+    /**
+     * 添付ファイル埋め込み用のPDF作成
+     *
+     * @param title
+     * @param description
+     * @return Path
+     */
+    public Path makeBasePdfFile(String title, String description) throws IOException {
+
+        // ディレクトリの作成
+        Files.createDirectories(this.tmpFileRootPath);
+
+        // 出力先ファイルパス
+        Path outputFilePath = this.tmpFileRootPath.resolve(
+                UUID.randomUUID().toString() + ".pdf");
+
+        // 出力用の文字列リストを作成
+        List<String> inputLines = List.of(
+            "[Date]", LocalDateTime.now().toString(), "",
+            "[Title]", title, "",
+            "[Description]", description
+        );
+
+        // PDDocumentおよびPDPageの初期化
+        PDDocument document = new PDDocument();
+        PDPage page = new PDPage();
+        document.addPage(page);
+
+        // フォント及びmediabox関連の変数初期化
+        PDFont pdfFont = PDType0Font.load(document, new File(jpFontPathStr));
+        float fontSize = 18;
+        float leading = 1.5f * fontSize;
+        float initMargin = 72;
+        float boxMargin = leading;
+        int lineCount = 0;
+
+        // add text table to PDFへのテキスト追加処理
+        try {
+            try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
+                for (String text : inputLines) {
+                    // mediaboxの初期化および開始位置の指定
+                    PDRectangle mediabox = page.getMediaBox();
+                    float width = mediabox.getWidth() - 2 * initMargin;
+                    float startX = mediabox.getLowerLeftX() + initMargin;
+                    float startY = mediabox.getUpperRightY() - initMargin - boxMargin * (lineCount + 1);
+
+                    // 複数行の折り返し処理
+                    List<String> lines = new ArrayList<String>();
+                    int lastSpace = -1;
+                    while (text.length() > 0) {
+                        int spaceIndex = text.indexOf(' ', lastSpace + 1);
+                        if (spaceIndex < 0) {
+                            spaceIndex = text.length();
+                        }
+                        String subString = text.substring(0, spaceIndex);
+                        float size = fontSize * pdfFont.getStringWidth(subString) / 1000;
+
+                        if (size > width) {
+                            if (lastSpace < 0) {
+                                lastSpace = spaceIndex;
+                            }
+                            subString = text.substring(0, lastSpace);
+                            lines.add(subString);
+                            text = text.substring(lastSpace).trim();
+                            lastSpace = -1;
+                        } else if (spaceIndex == text.length()) {
+                            lines.add(text);
+                            text = "";
+                        } else {
+                            lastSpace = spaceIndex;
+                        }
+                    }
+
+                    // PDFページへのテキスト追加
+                    contentStream.beginText();
+                    contentStream.setFont(pdfFont, fontSize);
+                    contentStream.newLineAtOffset(startX, startY);
+                    for (String line : lines) {
+                        contentStream.showText(line);
+                        contentStream.newLineAtOffset(0, -leading);
+                    }
+                    contentStream.endText();
+
+                    // 行カウントの加算(madiabox位置調整用)
+                    if (lines.size() == 0) {
+                        lineCount++;
+                    } else {
+                        lineCount += lines.size();
+                    }
+                }
+            }
+
+            // save PDF
+            document.save(outputFilePath.toString());
+            document.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // return path
+        return outputFilePath;
     }
 
     /**
